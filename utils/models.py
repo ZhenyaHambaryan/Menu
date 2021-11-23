0c3bae9e849de3c24e0c82c270d417da1836047c
ZhenyaHambaryan <j.hambaryan@mail.ru>
2021-11-23 14:44:34 +0400

    Add alt in Slaide

--- utils/models.py
+++ utils/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Slide(models.Model):
     image = models.ImageField(upload_to='uploads/', null=True, blank=True)
+    alt = models.CharField(null=True, max_length=255, blank=True)
 
 # Create your models here.
